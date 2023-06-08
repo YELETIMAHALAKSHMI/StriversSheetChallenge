a08720226998b13f84608864fbecdcf4ca3feebb
YELETIMAHALAKSHMI <94279266+YELETIMAHALAKSHMI@users.noreply.github.com>
2023-06-08 20:08:57 +0530

    Create removeDuplicates.java

---
+++ removeDuplicates.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Solution { 
+    public static int removeDuplicates(ArrayList<Integer> arr,int n) {
+         // Write your code here.  
+        int count=1;
+        for (int i=1;i<n;i++) {
+            if (!arr.get(i-1).equals(arr.get(i))) {
+                count++;
+            }
+        }
+    return count;
+
+   }
+
+ }
