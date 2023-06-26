fd911f9b76d2e6d67e3f915c7b8c444d703bfe1a
YELETIMAHALAKSHMI <94279266+YELETIMAHALAKSHMI@users.noreply.github.com>
2023-06-26 19:03:56 +0530

    Create maximumProductSubArray.py

---
+++ maximumProductSubArray.py
@@ -0,0 +1,13 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def maximumProduct(nums, n):
+    res = nums[0]
+    curr_max,curr_min = 1,1
+    for n in nums:
+        vals = (n, curr_max*n, curr_min*n)
+        curr_max,curr_min = max(vals),min(vals)
+        res = max(res,curr_max)
+    return res
